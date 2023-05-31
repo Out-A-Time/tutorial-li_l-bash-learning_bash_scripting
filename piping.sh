576c8006be79b09cb22e411ca2b398f2da0ecda8
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-05-31 20:17:11 +0100

    piping and redirections

---
+++ piping.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#Piping
+#This will send (pipe) text file to Word Count and will display
+#    11     951    7382   result tell us how many lines / words / characters text has.
+cat lorem.txt | wc 
+
+
+#Redirections
+#This will execute command 'ls -l' and redirect the output to text.txt file
+# total 32
+# -rw-r--r--  1 robert.maksymow62  staff   105 May 31 09:43 README.md
+# -rw-r--r--  1 robert.maksymow62  staff  7382 May 31 16:37 lorem.txt
+# -rwxr-xr-x  1 robert.maksymow62  staff   231 May 31 16:53 piping.sh
+# -rw-r--r--  1 robert.maksymow62  staff     0 May 31 16:54 text.txt
+
+ls -l > text.txt
+
