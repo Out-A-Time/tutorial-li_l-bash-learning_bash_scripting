edae90d3aa3205881140630ade7d48d7e0331e22
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-06-06 11:26:30 +0100

    Displaying text with 'echo'

---
+++ echo.sh
@@ -0,0 +1,21 @@
+echo hello
+echo hello world
+
+#variable
+worldsize=BIG 
+echo hello $worldsize world!
+
+#command substitution
+echo "The kernel is $(uname -r)."
+#command substitution with escaping characters
+echo The \(kernel\) is $(uname -r).
+#command substitution with single quotes (treats as literal text).
+echo 'The kernel is $(uname -r).'
+#command substitution with double quotes
+echo "The (kernel) is $(uname -r)."
+#mulitple lines
+echo; echo "More space!"; echo
+#no new lines (moves prompter to the end)
+echo -n "No new line"; echo
+#no new lines__(doesn't work, why???)
+echo -n "This is a part"; echo -n "of the same line";
